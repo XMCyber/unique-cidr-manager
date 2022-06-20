0fe0fbdb02e32616e183885b715c630e3db30112
Artiom Levinton <artiom@xmcyber.com>
2022-06-20 15:55:27 +0300

    add server files - initial commit

---
+++ server/main.py
@@ -0,0 +1,18 @@
+import time
+from http.server import HTTPServer
+from server import Server
+
+
+HOST_NAME = 'localhost'
+PORT_NUMBER = 8000
+
+if __name__ == '__main__':
+    httpd = HTTPServer((HOST_NAME, PORT_NUMBER), Server)
+    print(time.asctime(), 'Server UP - %s:%s' % (HOST_NAME, PORT_NUMBER))
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    print(time.asctime(), 'Server DOWN - %s:%s' % (HOST_NAME, PORT_NUMBER))
+    
