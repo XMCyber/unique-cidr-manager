85b67871a3af28d308d84e6562278da69eb81232
Artiom Levinton <artiom@xmcyber.com>
2023-02-20 13:35:37 +0200

    add button on frontend and new style

--- frontend/cidr.js
+++ frontend/cidr.js
@@ -1,4 +1,4 @@
-async function onSubmit() {
+async function get_cidr() {
 	const subnet_size = document.getElementById('subnet')?.value || '';
 	const requiredrange = document.getElementById('range')?.value || '';
 	const reason = document.getElementById('reason')?.value || '';
@@ -6,11 +6,25 @@ async function onSubmit() {
 	const url = `/get-cidr?subnet_size=${subnet_size}&requiredrange=${requiredrange}&reason=${reason}`;
 
 	try {
+		document.getElementById('cidr').value = "Retrieving unigue cidr..";
 		const response = await fetch(url);
 		const cidrRes = await response.text();
 		document.getElementById('cidr').value = (cidrRes);
 	}
 	catch (e) {
 		document.getElementById('cidr').value = `server error: ${e.message}`;
+	}
+}
+
+async function get_occupied_list() {
+	const url = `/get-occupied-list`;
+	try {
+		document.getElementById('cidrlist').value = "Trying to obtain list from GIT repo..";
+		const response = await fetch(url);
+		const occupiedlist = await response.text();
+		document.getElementById('cidrlist').value = (occupiedlist);
+	}
+	catch (e) {
+		document.getElementById('cidrlist').value = `server error: ${e.message}`;
 	}
 }
