8ce5b2b66a7b4b762578dd5f7248adb397417ebe
Artiom Levinton <artiom@xmcyber.com>
2023-11-26 20:35:18 +0200

    frontend-fixes

--- frontend/cidr.js
+++ frontend/cidr.js
@@ -1,3 +1,7 @@
+const git_message = 'Trying to obtain list from GIT repo';
+const wait_message = 'Please wait..';
+const error_message = 'Failed, please try again';
+
 async function get_cidr() {
 	const subnet_size = document.getElementById('subnet')?.value || '';
 	const requiredrange = document.getElementById('range')?.value || '';
@@ -7,6 +11,7 @@ async function get_cidr() {
 
 	try {
 		document.getElementById('cidr_messages').innerHTML = "Retrieving unigue cidr..";
+		document.getElementById('cidr_output').innerHTML = wait_message;
 		const response = await fetch(url);
 		const cidrRes = await response.text();
 		document.getElementById('cidr_output').innerHTML = (cidrRes);
@@ -14,14 +19,16 @@ async function get_cidr() {
 
 	}
 	catch (e) {
-		document.getElementById('cidr_messages').innerHTML = `server error: ${e.message}`;
+		document.getElementById('cidr_messages').innerHTML = `Server error: ${e.message}`;
+		document.getElementById('cidr_output').innerHTML = error_message;
 	}
 }
 
 async function get_occupied_list() {
 	const url = '/get-occupied-list';
 	try {
-	  document.getElementById('occupied_messages').innerHTML = 'Trying to obtain list from GIT repo..';
+	  document.getElementById('occupied_messages').innerHTML = git_message;
+	  document.getElementById('occupied_output').innerHTML = wait_message;
 	  const response = await fetch(url);
 	  const occupiedlist = await response.text();
 	  document.getElementById('occupied_output').innerHTML = occupiedlist;
@@ -29,6 +36,7 @@ async function get_occupied_list() {
 	  document.getElementById('occupied_messages').innerHTML = `Done! Raw count is ${num_elements}`;
 	} catch (e) {
 	  document.getElementById('occupied_messages').innerHTML = `Server error: ${e.message}`;
+	  document.getElementById('occupied_output').innerHTML = error_message;
 	}
 }
 
@@ -39,14 +47,17 @@ async function get_next_cidr() {
 
 	const url = `/get-next-cidr-no-push?subnet_size=${subnet_size}&requiredrange=${requiredrange}&reason=${reason}`;
 	try {
-		document.getElementById('cidr_messages').innerHTML = "Trying to obtain list from GIT repo..";
+		document.getElementById('cidr_messages').innerHTML = git_message;
+		document.getElementById('cidr_output').innerHTML = wait_message;
 		const response = await fetch(url);
 		const occupiedlist = await response.text();
 		document.getElementById('cidr_output').innerHTML = occupiedlist;
 		document.getElementById('cidr_messages').innerHTML = "Done!";
 	}
 	catch (e) {
-		document.getElementById('cidr_messages').innerHTML = `server error: ${e.message}`;
+		document.getElementById('cidr_messages').innerHTML = `Server error: ${e.message}`;
+		document.getElementById('cidr_output').innerHTML = error_message;
+
 	}
 }
 
@@ -55,12 +66,12 @@ async function delete_cidr_from_list() {
 
 	const url = `/delete-cidr-from-list?cidr_deletion=${cidr_deletion}`;
 	try {
-		document.getElementById('delete_messages').innerHTML = "Trying to obtain list from GIT repo..";
+		document.getElementById('delete_messages').innerHTML = git_message;
 		const response = await fetch(url);
 		const occupiedlist = await response.text();
 		document.getElementById('delete_messages').innerHTML = occupiedlist;
 	}
 	catch (e) {
-		document.getElementById('delete_messages').innerHTML = `server error: ${e.message}`;
+		document.getElementById('delete_messages').innerHTML = `Server error: ${e.message}`;
 	}
 }
