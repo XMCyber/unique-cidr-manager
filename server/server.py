44e2c6c7524376e886c753e208858d7e3f2b5a53
Artiom Levinton <artiom@xmcyber.com>
2024-08-19 11:11:35 +0200

    add server functions

--- server/server.py
+++ server/server.py
@@ -2,6 +2,8 @@ from http.server import BaseHTTPRequestHandler
 from urllib.parse import urlparse
 from cidr import get_cidr
 from subnet import subnets
+from rest_framework import status
+from rest_framework import status
 
 class Server(BaseHTTPRequestHandler): 
   def do_GET(self):
@@ -35,7 +37,17 @@ class Server(BaseHTTPRequestHandler):
       print(str(query))
       cidr_deletion = query_components["cidr_deletion"]
       self.respond_delete_cidr_from_list(cidr_deletion)
-   
+
+    if self.path.startswith("/add-cidr-manually"):
+      print("add-cidr-manually - Request is being served")
+      query = urlparse(self.path).query
+      query_components = dict(qc.split("=") for qc in query.split("&"))
+      print(str(query_components))
+      print(str(query))
+      cidr = query_components["cidr"]
+      reason = query_components["reason"]
+      self.respond_add_cidr_manually(cidr,reason)
+
     if self.path == "/":
       self.path = '/cidr.html'
       content = open('frontend'+self.path, 'rb').read()
@@ -110,7 +122,7 @@ class Server(BaseHTTPRequestHandler):
   def respond_get_occupied_list(self):
     content = self.handle_get_occupied_list(200, 'text/html')
     self.wfile.write(content)
-  
+
   #### get-next-cidr-no-push ####
   def handle_http_get_next_cidr_no_push(self, status, content_type, subnet_size,requiredrange,reason):
     self.send_response(status)
@@ -135,4 +147,17 @@ class Server(BaseHTTPRequestHandler):
   
   def respond_delete_cidr_from_list(self,cidr_deletion):
     content = self.handle_http_get_delete_cidr_from_list(200, 'text/html', cidr_deletion)
+    self.wfile.write(content)
+
+  #### add-cidr-manually ####
+  def handle_add_cidr_manually(self,cidr,reason):
+    self.send_response(status)
+    self.send_header('Content-type', content_type)
+    self.end_headers()
+    result=get_cidr.manually_add_cidr(cidr,reason)
+    print(str(result))
+    return bytes(str(result), "UTF-8")
+
+  def respond_add_cidr_manually(self,cidr,reason):
+    content = self.handle_add_cidr_manually(200, 'text/html', cidr,reason)
     self.wfile.write(content)
