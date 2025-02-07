9c0643a53983b14ef920de26a40aea42a7134614
Artiom Levinton <artiom@xmcyber.com>
2025-02-07 18:40:42 +0200

    final lint

--- server/subnet.py
+++ server/subnet.py
@@ -1,11 +1,14 @@
 from ipaddress import IPv4Network
 
-class subnets():
+
+class Subnets:
     
-    def get_subnets_from_cidr(subnet_size, cidr):
-        subnetslist = ""
+    def get_subnets_from_cidr(self, subnet_size, cidr):
+        subnets_list = []  # Using a list for better performance
         print("cidr: " + cidr)
         cidr = IPv4Network(cidr)
+        
         for subnet in cidr.subnets(new_prefix=int(subnet_size)):
-            subnetslist = subnetslist + str(subnet) + ' '
-        return subnetslist
+            subnets_list.append(str(subnet))
+        
+        return ' '.join(subnets_list)  # Joining the list into a single string
