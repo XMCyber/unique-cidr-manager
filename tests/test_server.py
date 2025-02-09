52dc6bfa77cd1c4a6c51213b09aabd7e82f06177
Artiom Levinton <artiom@xmcyber.com>
2025-02-09 12:41:24 +0200

    add-asserts-to-tests

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,5 +1,6 @@
 import unittest
 import requests
+from ipaddress import IPv4Network
 from time import sleep
 from subprocess import Popen, PIPE
 from datetime import datetime
@@ -19,25 +20,44 @@ class TestAPI(unittest.TestCase):
     def setUpClass(cls):
         # Start the server before tests
         cls.server_process = Popen(['python', 'server/main.py'])
-        sleep(2)  # Wait for the server to start
+        sleep(1)  # Wait for the server to start
 
     @classmethod
     def tearDownClass(cls):
         cls.server_process.terminate()  # Stop the server after tests
 
     def get_current_reason(self):
-        time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        time = datetime.now().strftime("%H:%M:%S")
         return f"test-{time}"
 
     def test_get_cidr(self):
         current_reason = self.get_current_reason()
-        response = requests.get(f"http://localhost:8000/get-cidr?subnet_size=24&requiredrange=192&reason={current_reason}")
+        response = requests.get(f"http://localhost:8000/get-cidr?subnet_size=26&requiredrange=10&reason={current_reason}")
         self.assertEqual(response.status_code, 200)
+        print(response.text)
+        self.assertTrue(IPv4Network(response.text))
+    
+    def test_get_subnets(self):
+        response = requests.get("http://localhost:8000/get-subnets?subnet_size=28&cidr=10.0.1.0/26")
+        self.assertEqual(response.status_code, 200)
+        print(response.text)
+        self.assertTrue(all(IPv4Network(subnet) for subnet in response.text.split()))
 
     def test_get_next_cidr_no_push(self):
         current_reason = self.get_current_reason()
-        response = requests.get(f"http://localhost:8000/get-next-cidr-no-push?subnet_size=24&requiredrange=192&reason={current_reason}")
+        response = requests.get(f"http://localhost:8000/get-next-cidr-no-push?subnet_size=26&requiredrange=10&reason={current_reason}")
         self.assertEqual(response.status_code, 200)
+        print(response.text)
+        self.assertTrue(IPv4Network(response.text))
+
+    def test_get_occupied_list(self):
+        response = requests.get(f"http://localhost:8000/get-occupied-list")
+        self.assertEqual(response.status_code, 200)
+        try:
+            json_data = response.json()
+            print
+        except ValueError:
+            self.fail("Response is not valid JSON")
 
 
 if __name__ == '__main__':
